c805d06047ed46886db5d54b4e2814dd4617fc7f
Sumanth-04 <sumanthsk04@gmail.com>
2020-08-10 15:27:19 +0530

    Dropdown component

--- src/App.js
+++ src/App.js
@@ -3,24 +3,25 @@ import "./App.css";
 import Drpdown from "./components/Drpdown";
 import Menu, { Item as MenuItem } from "rc-menu";
 
-function App() {
-  return (
-    <div className="App">
-      <br />
-      <Drpdown
-        className="Dropdown-className "
-        defaultVisible={true}
-        overlayClassName="dropdown-overlay"
-        showCaret={true}
-        /*visible={true}*/
-      >
-        <Menu>
-          <MenuItem key="1">1st item is Hi</MenuItem>
-          <MenuItem key="2">2nd item is Hello</MenuItem>
-        </Menu>
-      </Drpdown>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <br />
+        <Drpdown
+          className="Dropdown-className "
+          defaultVisible={true}
+          overlayClassName="dropdown-overlay"
+          showCaret={true}
+        >
+          <Menu>
+            <MenuItem key="1">1st item is Hi</MenuItem>
+            <MenuItem key="2">2nd item is Hello</MenuItem>
+          </Menu>
+        </Drpdown>
+      </div>
+    );
+  }
 }
 
 export default App;
